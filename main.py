0420db5e5f985df75f802d180f77205a002934a4
Alger Vuong <algervuong@gmail.com>
2017-04-25 14:02:55 -0700

    Frontend is connected with backend code

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import webapp2
-
+import json
+from contacts import *
 
 class Home(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        INDEX_HTML = open('index.html').read()
+        self.response.write(INDEX_HTML)
 
+class FormatContactNameRoute(webapp2.RequestHandler):
+    def post(self):
+        first = self.request.POST.get("first")
+        last = self.request.POST.get("last")
+        if(IsValidContactName(first, last)):
+            formattedName = FormatContactName(first, last)
+            self.response.write(unicode('Success! ' + first + ' and ' + last + ' are both valid! The formatted name is: ' + formattedName))
+        else:
+            self.response.write('Failure! Either ' + first + ' or ' + last + ' is invalid!')
 
 app = webapp2.WSGIApplication([
     ('/', Home),
+    ('/FormatContactNameRoute', FormatContactNameRoute),
 ], debug=True)
+
+# To run local server do python main.py
+def main():
+    from paste import httpserver
+    httpserver.serve(app, host='127.0.0.1', port='8080')
+if __name__ == '__main__':
+    main()
